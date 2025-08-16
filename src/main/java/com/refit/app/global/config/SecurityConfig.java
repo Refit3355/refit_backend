dc80fb0c4489ff11c13bd08b8d80ed9fae2c00af
Soojin-Lee-01 <sojinlee1004@naver.com>
2025-08-16 18:19:50 +0900

    [FEAT] 액세스 토큰 검사 filter 구현

--- src/main/java/com/refit/app/global/config/SecurityConfig.java
+++ src/main/java/com/refit/app/global/config/SecurityConfig.java
@@ -1,18 +1,24 @@
 package com.refit.app.global.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(
                                 "/api/auth/check/**",
