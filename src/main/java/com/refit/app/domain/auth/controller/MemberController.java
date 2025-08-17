f521c922eb1b190702173badb339126fa9d35fd1
Soojin-Lee-01 <sojinlee1004@naver.com>
2025-08-17 18:22:06 +0900

    [REFACTOR] 모든 엔드포인트에서 '/api' 프리픽스 제거

--- src/main/java/com/refit/app/domain/auth/controller/MemberController.java
+++ src/main/java/com/refit/app/domain/auth/controller/MemberController.java
@@ -25,7 +25,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/auth")
 @RequiredArgsConstructor
 public class MemberController {
 
