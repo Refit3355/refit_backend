8f30ff77da32c652b21cacc54a590aa9e870c044
Soojin-Lee-01 <sojinlee1004@naver.com>
2025-08-31 10:43:14 +0900

    [FEAT] 성분 규칙 DB 조회 쿼리 작성 (#54)

--- src/main/java/com/refit/app/domain/analysis/mapper/AnalysisMapper.java
+++ src/main/java/com/refit/app/domain/analysis/mapper/AnalysisMapper.java
@@ -4,6 +4,9 @@ import com.refit.app.domain.analysis.dto.AnalysisHairConcernDto;
 import com.refit.app.domain.analysis.dto.AnalysisHealthConcernDto;
 import com.refit.app.domain.analysis.dto.AnalysisHealthInfoDto;
 import com.refit.app.domain.analysis.dto.AnalysisSkinConcernDto;
+import com.refit.app.domain.analysis.dto.IngredientRule;
+import java.util.Collection;
+import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
@@ -17,4 +20,8 @@ public interface AnalysisMapper {
     AnalysisHealthConcernDto selectHealthConcern(@Param("memberId") Long memberId);
 
     AnalysisHairConcernDto selectHairConcern(@Param("memberId") Long memberId);
+
+    List<IngredientRule> selectByNames(@Param("names") Collection<String> names);
+  
+    String selectMemberNickname(@Param("memberId") Long memberId);
 }
